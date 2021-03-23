a76049362b5580c59c096aff147951cbbb482c16
Michał Mroczkowski <mchlmroczkowski@gmail.com>
2021-03-23 23:43:34 +0100

    Add counting module

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ shopping_dict = {
 k=0
 print('Lista zakupów')
 for r in shopping_dict :
+     k = k + len(shopping_dict[r])
+    print(f'Idę do {str.capitalize(r)} kupić {shopping_dict[r]}')
+print(f'Kupiono {k} produktów')   
